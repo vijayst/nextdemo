29d1ab480c47b3d4ce3243676e6110abafd34a1b
vijayst <vijayst@gmail.com>
2019-02-16 16:00:39 +0530

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+import styles from './index.css';
+
 export default function Index() {
-    return <div>Hello world</div>;
+    return (
+        <div className={styles.home}>
+            <div className={styles.center}>
+                <div className={styles.company}>Vijay Consulting Services</div>
+                <p className={styles.offering}>
+                    We offer custom application development using React
+                    framework
+                </p>
+                <Link href="/services">
+                    <button className={styles.button}>View our services</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
