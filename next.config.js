bf42be328438fb768a8036247de42bc8d1c94279
vijayst <vijayst@gmail.com>
2019-02-16 19:24:39 +0530

    export dynamic pages as static files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,36 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
     cssModules: true,
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' },
+            '/services': { page: '/services' },
+            '/services/react-app': {
+                page: '/service',
+                query: {
+                    slug: 'react-app'
+                }
+            },
+            '/services/react-native-app': {
+                page: '/service',
+                query: {
+                    slug: 'react-native-app'
+                }
+            },
+            '/services/nextjs-app': {
+                page: '/service',
+                query: {
+                    slug: 'nextjs-app'
+                }
+            },
+            '/services/gatsby-app': {
+                page: '/service',
+                query: {
+                    slug: 'gatsby-app'
+                }
+            }
+        };
+    },
     webpack: function(config) {
         config.module.rules.push({
             test: /\.md$/,
