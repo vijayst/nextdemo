960267c278de33606b765c561e4c12cc01ab4f51
vijayst <vijayst@gmail.com>
2019-02-16 19:12:16 +0530

    service page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
-    cssModules: true
+    cssModules: true,
+    webpack: function(config) {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader'
+        });
+        return config;
+    }
 });
--- pages/services.js
+++ pages/services.js
@@ -9,10 +9,10 @@ export default function Services() {
                 <h1>Services</h1>
                 <p>View our service offerings:</p>
                 <ul className={styles.services}>
-                    <li><Link href="/service/react-app"><a>React apps</a></Link></li>
-                    <li><Link href="/service/react-native-app"><a>React Native apps</a></Link></li>
-                    <li><Link href="/service/nextjs-app"><a>Next apps</a></Link></li>
-                    <li><Link href="/service/gatsby-app"><a>Gatsby apps</a></Link></li>
+                    <li><Link as="/services/react-app" href="/service?slug=react-app"><a>React apps</a></Link></li>
+                    <li><Link as="/services/react-native-app" href="/service?slug=react-native-app"><a>React Native apps</a></Link></li>
+                    <li><Link as="/services/nextjs-app" href="/service?slug=nextjs-app"><a>Next apps</a></Link></li>
+                    <li><Link as="/services/gatsby-app" href="/service?slug=gatsby-app"><a>Gatsby apps</a></Link></li>
                 </ul>
             </div>
         </Layout>
