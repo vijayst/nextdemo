5e12a3e144a2163c3a6ff837bc252a55bc666386
vijayst <vijayst@gmail.com>
2019-02-16 17:24:50 +0530

    services page

---
+++ components/layout.js
@@ -0,0 +1,33 @@
+import styles from './layout.css';
+import Link from 'next/link';
+
+export default function Layout(props) {
+    return (
+        <div>
+            <header className={styles.header}>
+                <div className={styles.wrapper}>
+                    <h1>
+                        <Link href="/">
+                            <a className={styles.titleLink}>
+                                Vijay Consulting Services
+                            </a>
+                        </Link>
+                    </h1>
+                    <div>
+                        <ul className={styles.menu}>
+                            <li>
+                                <Link href="/"><a>Home</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/services"><a>Services</a></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+            <main>
+                {props.children}
+            </main>
+        </div>
+    );
+}
